e2b7c375221bed895a51dd8a86a184b5e668bd68
Tony Boswell <tboswell@imodules.com>
2025-01-03 14:43:38 -0600

    Fetch latest products for homepage; replace sample data with dynamic data retrieval

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,12 @@
-import sampleData from "@/db/sample-data";
 import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
 
-const Homepage = () => {
- const products = sampleData.products;
+const Homepage = async () => {
+ const latestProducts = await getLatestProducts();
+
   return (
     <>
-      <ProductList data={products} title="Newest Arrivals" limit={4}/>
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={4}/>
     </> 
   );
 }
