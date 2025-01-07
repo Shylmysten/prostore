4366cbe8554ba4c4414a547d5c89143cf06f030c
Tony Boswell <tboswell@imodules.com>
2025-01-07 16:13:28 -0600

    Implement credential-based sign-in form with action handling and callback URL support; update dependencies to specific versions

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -2,12 +2,36 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 import { signInDefaultValues } from "@/lib/constants";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 
 const CredentialsSignInForm = () => {
+    const [data, action] = useActionState(signInWithCredentials, {
+        success: false,
+        message: ''
+    })
+
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+    const SignInButton = () => {
+        const { pending } = useFormStatus();
+
+        return (
+            <Button disabled={pending} className="w-full" variant='default'>
+                { pending ? 'Signing in...' : 'Sign In' }
+            </Button>
+        )
+    }
+
+ 
     return (
-        <form>
+        <form action={action}>
+            <input type="hidden" name="callbackUrl" value={callbackUrl} />
             <div className="space-y-6">
                 <div>
                     <Label htmlFor='email'>Email</Label>
@@ -17,11 +41,18 @@ const CredentialsSignInForm = () => {
             <div className="space-y-6">
                 <div>
                     <Label htmlFor='password'>Password</Label>
-                    <Input id='password' name='password' type='password' required/>
+                    <Input id='password' name='password' type='password' required autoComplete="password" defaultValue={signInDefaultValues.password}/>
                 </div>
                 <div>
-                    <Button className="w-full" variant='default'>Sign In</Button>
+                   <SignInButton />
                 </div>
+
+                {data && ! data.success && (
+                    <div className="text-center text-destructive">
+                        { data.message }
+                    </div>
+                )}
+
                 <div className="text-sm text-center text-muted-foreground">
                     Don&apos;t Have an Account?{' '}
                     <Link href='/sign-up' target='_self'>Sign Up</Link>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { signIn, signOut } from "@/auth";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(prevState: unknown, formData: FormData) {
+    
     try {
         const user = signInFormSchema.parse({
             email: formData.get('email'),
@@ -16,6 +17,7 @@ export async function signInWithCredentials(prevState: unknown, formData: FormDa
         return { success: true, message: 'Signed in successfully' }
 
     } catch (error) {
+        console.log(formData)
         if(isRedirectError(error)) {
             throw error;
         }
