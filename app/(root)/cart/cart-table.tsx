e33b8ff997f65d577f0277662cc736a2f02f09f0
Tony Boswell <tboswell@imodules.com>
2025-01-17 10:08:20 -0600

    Add subtotal display and checkout button to CartTable component

--- app/(root)/cart/cart-table.tsx
+++ app/(root)/cart/cart-table.tsx
@@ -7,7 +7,8 @@ import { ArrowRight, Loader, Minus, Plus } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from "@/components/ui/table";
-
+import { Card, CardContent } from "@/components/ui/card";
+import { formatCurrency } from "@/lib/utils";
 import { Cart } from "@/types";
 import { Button } from "@/components/ui/button";
 
@@ -85,6 +86,22 @@ const CartTable = ({ cart}: { cart?: Cart }) => {
                             </TableBody>
                         </Table>
                     </div>
+                    <Card>
+                        <CardContent className="p-4 gap-4">
+                            <div className="pb-3 text-xl">
+                                SubTotal ({ cart.items.reduce((a,c) => a + c.qty, 0 )}):
+                                <span className="font-bold">{ formatCurrency(cart.itemsPrice)}</span>
+                            </div>
+                            <Button className="w-full" disabled={isPending} onClick={() => startTransition(() => router.push('/shipping-address'))}>
+                                { isPending ? (
+                                    <Loader className="w-4 h-4 animate-spin" />
+                                ) : (
+                                    <ArrowRight className="w-4 h-4" />
+                                )}
+                                {' '} Proceed to Checkout
+                            </Button>
+                        </CardContent>
+                    </Card>
                 </div>
             ) }
         </> 
