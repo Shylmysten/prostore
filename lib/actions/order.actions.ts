583b1b1540750979b5dd37959f03d1f711a3fbd1
Tony Boswell <tboswell@imodules.com>
2025-02-01 13:52:11 -0600

    Add Order Details page and getOrderById function; update Order model and migration

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,7 +1,7 @@
-'use server';
+'user server'
 
 import { isRedirectError } from "next/dist/client/components/redirect";
-import { formatError } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { auth } from "@/auth";
 import { getMyCart } from "./cart.actions";
 import { getUserById } from "./user.actions";
@@ -103,4 +103,24 @@ export async function createOrder() {
             message: formatError(error),
         }
     }
+}
+
+// Get Order by Id
+export async function getOrderById(orderId: string) {
+    const data = await prisma.order.findFirst({
+        where: { 
+            id: orderId 
+        },
+        include: { 
+            orderitems: true,
+            user: { 
+                select: { 
+                    email: true,
+                    name: true,
+                } 
+            },
+        },
+    });
+
+    return convertToPlainObject(data);
 }
