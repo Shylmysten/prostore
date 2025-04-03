9bdfff17d617e8dddbf4ec5ea99d7977705f029b
Tony Boswell <tboswell@imodules.com>
2025-04-03 13:06:27 -0500

    Add product creation and update functionality with validation in admin panel

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -25,3 +25,18 @@ export const PAYEMENT_METHODS = process.env.PAYEMENT_METHODS ? process.env.PAYEM
 export const DEFAULT_PAYEMENT_METHOD = process.env.DEFAULT_PAYEMENT_METHOD || 'PayPal';
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 12;
+
+export const productDefaultValues = {
+    name: '',
+    slug: '',
+    category: '',
+    images: [],
+    brand: '',
+    description: '',
+    price: '0',
+    stock: '0',
+    rating: '0',
+    numReviews: '0',
+    isFeatured: false,
+    banner: null,
+}
