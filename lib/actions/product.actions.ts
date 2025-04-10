768a3df14f9bea36d3a60e7920c51867a191b91f
Tony Boswell <tboswell@imodules.com>
2025-04-10 09:28:12 -0500

    feat: add search functionality to admin pages and update order/user/product queries

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -6,6 +6,7 @@ import { convertToPlainObject, formatError } from "../utils";
 import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import { z } from 'zod';
+import { Prisma } from '@prisma/client';
 // Get Latest Products
 export async function getLatestProducts() {
   
@@ -57,7 +58,21 @@ export async function getAllProducts({
     page: number;
     category?: string
 }) {
+    // Query filter
+    const queryFilter: Prisma.ProductWhereInput =
+        query && query !== 'all'
+        ? {
+            name: {
+            contains: query,
+            mode: 'insensitive',
+            } as Prisma.StringFilter,
+        }
+        : {};
+
     const data = await prisma.product.findMany({
+        where: {
+            ...queryFilter,
+        },
         orderBy: {createdAt: 'desc'},
         skip: (page - 1) * limit,
         take: limit
