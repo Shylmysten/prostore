64b0b6feb6fece2f018f833c17651b83c5008d90
Tony Boswell <tboswell@imodules.com>
2025-04-14 11:20:15 -0500

    feat: add URL construction for search filters in SearchPage component

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -20,6 +20,33 @@ const SearchPage = async (props: {
         page = '1',
     } = await props.searchParams;
 
+    // Construct filter url
+    const getFilterUrl = ({
+        c,
+        p,
+        s,
+        r,
+        pg
+    }: {
+        c?: string;
+        p?: string;
+        s?: string;
+        r?: string;
+        pg?: string;
+    }) => {
+        
+        const params = { q, category, price, rating, sort, page };
+
+        if(c) params.category = c;
+        if(p) params.price = p;
+        if(s) params.sort = s;
+        if(r) params.rating = r;
+        if(pg) params.page = pg;
+
+        return `/search?${new URLSearchParams(params)}`
+
+    }
+
     const products = await getAllProducts({ 
         query: q,
         category,
@@ -33,7 +60,7 @@ const SearchPage = async (props: {
     return ( 
         <div className="grid md:grid-cols-5 md:gap-5">
             <div className="filter-links">
-                {/* FILTERS */}
+                URL: { getFilterUrl({c: 'Mens Sweat Shirts'})}
             </div>
             <div className="md:col-span-4 space-y-4">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
